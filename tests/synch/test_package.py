bac4d1bea1301f3ace8b8ba2d48dc5c3a907a73d
Miguel Ferreira <miguelferreira@me.com>
2013-07-31 11:22:14 +0200

    Rename test folder to tests. Create synch package under it with some tests for the seflabtools.synch package code

---
+++ tests/synch/test_package.py
@@ -0,0 +1,30 @@
+'''
+Created on Jul 31, 2013
+
+@author: mferreira
+'''
+import unittest
+import sys
+from cStringIO import StringIO
+
+import synch
+
+class SynchPackageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.old_stdout = sys.stdout
+        sys.stdout = self.mystdout = StringIO()
+        
+    def tearDown(self):
+        sys.out = self.old_stdout
+        
+    def testPrintUsage(self):
+        synch.printUsage("cmd")
+        output = self.mystdout.getvalue()
+        
+        self.assertIsNotNone(output)
+        self.assertNotEqual("", output)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
